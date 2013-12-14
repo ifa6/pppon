0bc7f925a67ede12443359071cbec1898b12401e
José Lorenzo Nieto Corral <joselo.prbg@gmail.com>
2013-12-14 15:18:21 +0100

    Primer, y puede que único, commit

---
+++ ppp-on.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+LOCAL=${HOME}/.local
+RUNDIR=${LOCAL}/var/run
+RUNFILE=${RUNDIR}/ppp-on.sh_is_running
+
+on () {
+    touch $RUNFILE 2>/dev/null
+}
+
+off () {
+    rm -f $RUNFILE 2>/dev/null
+}
+
+hay_un_script () {
+    local x
+    x=`ls $RUNDIR | grep ''$RUNFILE''`
+    (( ${#x} > 0 ))
+}
+
+hay_conexion () {
+    local x
+    x=`/sbin/ifconfig ppp0 2>/dev/null | grep 'RUNNING' | wc -l`
+    (( x == 1 ))
+}
+
+hay_un_chat () {
+    local x
+    x=`pidof chat`
+    (( ${#x} > 0 ))
+}
+
+
+# --
+
+hay_un_script && exit 0
+
+on
+until hay_conexion
+do
+    killall pppd && sleep 1
+    pon provider && sleep 1
+    while hay_un_chat; do sleep 5; done
+done
+off
+
+exit 0
+
